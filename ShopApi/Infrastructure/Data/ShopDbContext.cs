cfb5008cb829907fbe787fd84f63bec2ead84510
Farzne Akbari <akbari76.f@gmail.com>
2026-07-14 16:38:46 +0330

    user entity

--- ShopApi/Infrastructure/Data/ShopDbContext.cs
+++ ShopApi/Infrastructure/Data/ShopDbContext.cs
@@ -11,6 +11,7 @@ public class ShopDbContext : DbContext
     }
 
     public DbSet<Product> Products => Set<Product>();
+    public DbSet<User> Users => Set<User>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
